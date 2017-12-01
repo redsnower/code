6c50fdd74e344c16692b082ffb466f7f3bee1459
redsnower <hongriliang@163.com>
2017-12-01 09:39:25 +0800

    Update ReverseBusinessDemo.java

--- wxpay_scanpay_java_demo/src/main/java/com/tencent/business/ReverseBusinessDemo.java
+++ wxpay_scanpay_java_demo/src/main/java/com/tencent/business/ReverseBusinessDemo.java
@@ -15,17 +15,17 @@ public class ReverseBusinessDemo {
 
         WXPay.initSDKConfiguration(
         		//签名算法需要用到的秘钥
-        		"sunrui8763C3360c4908659048569034",
+        		"1",
         		//公众账号ID，成功申请公众账号后获得
-        		"wx40aa3cd79125e0e5",
+        		"1",
         		//商户ID，成功申请微信支付功能之后通过官方发出的邮件获得
-        		"1410780102",
+        		"1",
         		//子商户ID，受理模式下必填；
         		"",
         		//HTTP证书在服务器中的路径，用来加载证书用
         		"C:/cert/apiclient_cert.p12",
         		//HTTP证书的密码，默认等于MCHID
-        		"1410780102"
+        		"1"
         		);
         ReverseReqData reverseReqData=new ReverseReqData("","ke7us2wre");
       		String string=WXPay.requestReverseService(reverseReqData);
